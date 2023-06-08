6885d09a3e87a359c925a38b8a2f193a62549fd9
Adio3 <a.awwal@native.semicolon.africa>
2023-06-08 17:33:05 +0100

    initial commit

---
+++ introduction/introduction/values.js
@@ -0,0 +1,14 @@
+let x = 10;
+let y = 5;
+
+let sumX = x * x;
+let sumY = y * y;
+
+let sum1 = sumX / 10;
+let sum2 = sumY / 10;
+
+x = sum1;
+y = sum2;
+
+console.log(x);
+console.log(y);
---
+++ introduction/introduction/variable.js
@@ -0,0 +1,44 @@
+var number = 20;
+let age = 23;
+const SCHOOL = "Semicolon Africa";
+
+console.log(age);
+console.log(number);
+console.log(SCHOOL);
+
+var a = 50
+var b = 500;
+var c = 100
+
+a = b;
+b = c;
+c = a;
+
+console.log(true && true)
+console.log(false && !false)
+console.log(false || true)
+console.log(false && true)
+
+var condition = false
+condition ? console.log("false") : console.log("true")
+
+let one = 2;
+let two = 4;
+let three= 6
+let temp = one;
+
+
+one = two
+two = three
+three = temp
+
+
+
+
+console.log(one)
+console.log(two)
+console.log(temp)
+
+console.log(a);
+console.log(b);
+console.log(c);
